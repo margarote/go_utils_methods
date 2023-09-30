2f21a6c414c97ff8320466151d129a9102c71ec2
margarote <joaovitortur1@gmail.com>
2023-09-30 10:13:21 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go.utils.methods
+
+go 1.21.0
---
+++ methods/methods.go
@@ -0,0 +1,154 @@
+package methods
+
+func Map[T comparable, V comparable](slice []T, f func(param T) V) []V {
+
+	newList := []V{}
+	for _, v := range slice {
+		newList = append(newList, f(v))
+	}
+
+	return newList
+}
+
+func Filter[T comparable](slice []T, f func(param T) bool) []T {
+
+	newList := []T{}
+	for _, v := range slice {
+		if f(v) {
+			newList = append(newList, v)
+		}
+	}
+
+	return newList
+}
+
+func Reduce[T comparable, V comparable](slice []T, f func(param T) V) V {
+
+	var result V
+	for _, v := range slice {
+		result = f(v)
+	}
+
+	return result
+}
+
+func Find[T comparable](slice []T, f func(param T) bool) []T {
+
+	newList := []T{}
+
+	for _, v := range slice {
+		if f(v) {
+			newList = append(newList, v)
+		}
+	}
+
+	return newList
+}
+
+func FindFirst[T comparable](slice []T, f func(param T) bool) T {
+
+	for _, v := range slice {
+		if f(v) {
+			return v
+		}
+	}
+
+	return slice[0]
+}
+
+func FindLast[T comparable](slice []T, f func(param T) bool) []T {
+
+	newList := []T{}
+
+	for i := len(slice) - 1; i >= 0; i-- {
+		if f(slice[i]) {
+			newList = append(newList, slice[i])
+		}
+	}
+
+	return newList
+}
+
+func FindIndex[T comparable](slice []T, f func(param T) bool) int {
+
+	for i, v := range slice {
+		if f(v) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func FindLastIndex[T comparable](slice []T, f func(param T) bool) int {
+
+	for i := len(slice) - 1; i >= 0; i-- {
+		if f(slice[i]) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func Every[T comparable](slice []T, f func(param T) bool) bool {
+
+	for _, v := range slice {
+		if !f(v) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Some[T comparable](slice []T, f func(param T) bool) bool {
+
+	for _, v := range slice {
+		if f(v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IndexOf[T comparable](slice []T, f func(param T) bool) int {
+
+	for i, v := range slice {
+		if f(v) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func LastIndexOf[T comparable](slice []T, f func(param T) bool) int {
+
+	for i := len(slice) - 1; i >= 0; i-- {
+		if f(slice[i]) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func Includes[T comparable](slice []T, f func(param T) bool) bool {
+
+	for _, v := range slice {
+		if f(v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ForEach[T comparable](slice []T, f func(param T)) {
+
+	for _, v := range slice {
+		f(v)
+	}
+}
