88129ff248fc520c51e0d4dbff789aab96e20298
margarote <joaovitortur1@gmail.com>
2023-09-30 10:46:40 -0300

    add

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/margarote/go_utils_methods
 
 go 1.21.0
+
+require go.mongodb.org/mongo-driver v1.12.1
--- methods/methods.go
+++ methods/methods.go
@@ -1,5 +1,11 @@
 package methods
 
+import (
+	"encoding/json"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 func Map[T comparable, V comparable](slice []T, f func(param T) V) []V {
 
 	newList := []V{}
@@ -151,4 +157,36 @@ func ForEach[T comparable](slice []T, f func(param T)) {
 	for _, v := range slice {
 		f(v)
 	}
+}
+
+func ConvertJsonInObjectMongo(val interface{}, convert interface{}) error {
+	jsonData, err := bson.MarshalExtJSON(val, false, false)
+
+	if err != nil {
+		return err
+	}
+
+	err = bson.UnmarshalExtJSON(jsonData, false, convert)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ConvertJsonInObject(val interface{}, convert interface{}) error {
+	jsonData, err := json.Marshal(val)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(jsonData, convert)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
